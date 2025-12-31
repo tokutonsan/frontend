24deae4d1274a40bae7dd430cc8ad6f2f3c51c09
tomoe <tomochankonabejapan@gmail.com>
2025-12-31 18:47:30 +0900

    yml修正12

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone", // ← ★これを追加npm run build
+  
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.API_ENDPOINT,
